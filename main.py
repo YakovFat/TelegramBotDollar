fe4f64e9bbc5f469daab04f8cb6aa5c91b2780d7
Yakov <yakovfat94@gmail.com>
2020-02-09 11:15:40 +0300

    сделал вывод текущего курса доллара по команде

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
 import logging
-import requests
-from bs4 import BeautifulSoup
 from aiogram import Bot, Dispatcher, executor, types
-
-
-API_TOKEN = ''
-PROXY_URL = ''
-
+from key_bot import API_TOKEN, PROXY_URL
+from forex_python.converter import CurrencyRates
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
 # Initialize bot and dispatcher
-bot = Bot(token=API_TOKEN, proxy=PROXY_URL)
+# bot = Bot(token=API_TOKEN, proxy=PROXY_URL)
+bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=['start', 'help'])
+@dp.message_handler(commands=['usd'])
 async def send_welcome(message: types.Message):
-    """
-    This handler will be called when user sends `/start` or `/help` command
-    """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+    c = CurrencyRates()
+    usd = c.get_rate('USD', 'RUB')
+    await message.reply(usd)
 
 
 if __name__ == '__main__':
